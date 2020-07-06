e1474bd4eb510fe6cea02b1fcb4573bb8b030994
git-hgjeong <code.hgjeong@gmail.com>
2020-07-06 20:03:37 +0900

    add Terms of Use

--- app/src/main/java/com/example/passover/TermsFragment.kt
+++ app/src/main/java/com/example/passover/TermsFragment.kt
@@ -6,29 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class TermsFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_terms, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
-        view.findViewById<Button>(R.id.btn1_2).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_TermsFragment)
+        view.findViewById<Button>(R.id.btnTermsBack).setOnClickListener {
+            findNavController().navigate(R.id.action_TermsFragment_to_SecondFragment)
         }
     }
 }
